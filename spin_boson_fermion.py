5e93ef6bfeddad9bf25752c7b60e6e2354c85262
marukatsutech <marukatsutech@gmail.com>
2024-04-13 21:49:13 +0900

    Update rotation option. and rename filename.

--- spin_boson_fermion.py
+++ spin_boson_fermion.py
@@ -1,33 +1,34 @@
-# Spin of fermion
+# Spin of boson and fermion
 import numpy as np
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import tkinter as tk
 from tkinter import ttk
-from matplotlib.patches import Circle
 from scipy.spatial.transform import Rotation
 import mpl_toolkits.mplot3d.art3d as art3d
 from mpl_toolkits.mplot3d import proj3d
 
 
-def set_spin_n(value):
-    global spin_numerator
-    spin_numerator = int(value)
+def change_spin_state():
+    global rot_xy, rot_txy
+    if var_chk_axis_ts.get():
+        rot_txy = 1
+    else:
+        rot_txy = 0
+    if var_chk_axis_ss.get():
+        rot_xy = 1
+    else:
+        rot_xy = 0
     reset()
     update_diagram()
-
-
-def set_spin_d(value):
-    global spin_denominator
-    spin_denominator = int(value)
-    reset()
-    update_diagram()
+    draw_pass_spin()
 
 
 def draw_pass_spin():
     global x_light_arrow_pass, y_light_arrow_pass, z_light_arrow_pass
     global theta_rad_spin_axis, theta_rad_light_arrow
+    global spin_pass_additional
     x_light_arrow_pass = []
     y_light_arrow_pass = []
     z_light_arrow_pass = []
@@ -43,17 +44,33 @@ def draw_pass_spin():
         x_light_arrow_pass.append(u1)
         y_light_arrow_pass.append(v1)
         z_light_arrow_pass.append(w1)
-        theta_rad_spin_axis = theta_rad_spin_axis - spin_numerator * ((2. * np.pi) / 360)
-        theta_rad_light_arrow = theta_rad_light_arrow - spin_denominator * ((2. * np.pi) / 360)
+        theta_rad_spin_axis = theta_rad_spin_axis - rot_xy * ((2. * np.pi) / 360)
+        theta_rad_light_arrow = theta_rad_light_arrow + rot_txy * ((2. * np.pi) / 360)
     plt_light_arrow_pass.set_xdata(np.array(x_light_arrow_pass))
     plt_light_arrow_pass.set_ydata(np.array(y_light_arrow_pass))
     plt_light_arrow_pass.set_3d_properties(np.array(z_light_arrow_pass))
+    # Spin pass additional
+    for j in range(7):
+        rot_matrix_z_ad = Rotation.from_rotvec(((j + 1) * np.pi / 180. * 360. / 8.) * vector_z_axis)
+        x_light_arrow_pass_ad = []
+        y_light_arrow_pass_ad = []
+        z_light_arrow_pass_ad = []
+        for k in range(360):
+            vector_original = np.array([x_light_arrow_pass[k], y_light_arrow_pass[k], z_light_arrow_pass[k]])
+            vector_ad = rot_matrix_z_ad.apply(vector_original)
+            u_ad, v_ad, w_ad = vector_ad[0], vector_ad[1], vector_ad[2]
+            x_light_arrow_pass_ad.append(u_ad)
+            y_light_arrow_pass_ad.append(v_ad)
+            z_light_arrow_pass_ad.append(w_ad)
+        spin_pass_additional[j].set_xdata(np.array(x_light_arrow_pass_ad))
+        spin_pass_additional[j].set_ydata(np.array(y_light_arrow_pass_ad))
+        spin_pass_additional[j].set_3d_properties(np.array(z_light_arrow_pass_ad))
 
 
 def update_diagram():
     global plt_guide_circle, theta_rad_spin_axis_anim, theta_rad_light_arrow_anim
     global qvr_spin_axis, qvr_light_arrow
-    draw_pass_spin()
+    # draw_pass_spin()
     # Rotation matrix (z axis)
     rot_matrix_z = Rotation.from_rotvec(theta_rad_spin_axis_anim * vector_z_axis)
     # Guide circle
@@ -85,9 +102,6 @@ def update_diagram():
     u1, v1, w1 = vector_rotated_light_arrow[0], vector_rotated_light_arrow[1], vector_rotated_light_arrow[2]
     qvr_light_arrow = ax0.quiver(x1, y1, z1, u1, v1, w1, length=1, color='darkorange', normalize=True,
                                  label='Phase(light arrow)')
-    # Change theta
-    theta_rad_spin_axis_anim = theta_rad_spin_axis_anim - spin_numerator * ((2. * np.pi) / 360)
-    theta_rad_light_arrow_anim = theta_rad_light_arrow_anim - spin_denominator * ((2. * np.pi) / 360)
 
 
 def reset():
@@ -110,10 +124,17 @@ def switch():
 
 def update(f):
     global cnt
+    global theta_rad_spin_axis_anim, theta_rad_light_arrow_anim
     txt_step.set_text("Step=" + str(cnt))
-    txt_spin.set_text("Spin " + str(spin_numerator) + "/" + str(spin_denominator))
+    if rot_txy != 0:
+        txt_spin.set_text("Spin " + str(1) + "/" + str(rot_txy + rot_xy))
+    else:
+        txt_spin.set_text("Not available")
     if is_play:
         update_diagram()
+        # Change theta
+        theta_rad_spin_axis_anim = theta_rad_spin_axis_anim - rot_xy * ((2. * np.pi) / 360)
+        theta_rad_light_arrow_anim = theta_rad_light_arrow_anim + rot_txy * ((2. * np.pi) / 360)
         cnt += 1
 
 
@@ -141,11 +162,11 @@ theta_rad_light_arrow_anim = 0.
 
 step_rotation = 0.1
 
-spin_numerator = 1
-spin_denominator = 1
+rot_xy = 1
+rot_txy = 1
 
 # Generate figure and axes
-title_ax0 = "Spin of fermion"
+title_ax0 = "Spin of boson and fermion"
 title_tk = title_ax0
 x_min = range_x_min
 x_max = range_x_max
@@ -170,7 +191,7 @@ ax0.set_zlim(z_min, z_max)
 txt_step = ax0.text2D(x_min, y_max, "Step=" + str(0))
 xz, yz, _ = proj3d.proj_transform(x_min, y_max, z_max, ax0.get_proj())
 txt_step.set_position((xz, yz))
-txt_spin = ax0.text2D(x_min, y_max, "Spin " + str(spin_numerator) + "/" + str(spin_denominator), fontsize=24)
+txt_spin = ax0.text2D(x_min, y_max, "Spin " + str(1) + "/" + str(1), fontsize=24)
 xz, yz, _ = proj3d.proj_transform(x_min, y_min, z_max, ax0.get_proj())
 txt_spin.set_position((xz, yz))
 
@@ -193,13 +214,13 @@ angle_guide_circle = np.arange(0., 360., 1.)
 x_guide_circle = np.cos(angle_guide_circle * np.pi / 180.)
 y_guide_circle = angle_guide_circle * 0.
 z_guide_circle = np.sin(angle_guide_circle * np.pi / 180.)
-plt_guide_circle, = ax0.plot(x_guide_circle, y_guide_circle, z_guide_circle, linewidth=1, linestyle=':', c='darkorange')
+plt_guide_circle, = ax0.plot(x_guide_circle, y_guide_circle, z_guide_circle, linewidth=2, linestyle=':', c='darkorange')
 
 # Spin axis arrow
 x0_, y0_, z0_ = 0., 0., 0.
 u0_, v0_, w0_ = vector_spin_axis_initial[0], vector_spin_axis_initial[1], vector_spin_axis_initial[2]
 qvr_spin_axis = ax0.quiver(x0_, y0_, z0_, u0_, v0_, w0_, length=1, color='blue', normalize=True,
-                           label='Axis of spin')
+                           label='Rotation of light circle')
 
 # Light arrow
 x1_, y1_, z1_ = 0., 0., 0.
@@ -207,8 +228,20 @@ u1_, v1_, w1_ = vector_light_arrow_initial[0], vector_light_arrow_initial[1], ve
 qvr_light_arrow = ax0.quiver(x1_, y1_, z1_, u1_, v1_, w1_, length=1, color='darkorange', normalize=True,
                              label='Phase(light arrow)')
 
+# Spin pass additional
+x_light_arrow_pass = []
+y_light_arrow_pass = []
+z_light_arrow_pass = []
+spin_pass_additional = []
+for i_ in range(7):
+    plt_light_arrow_pass_ad, = ax0.plot(np.array(x_light_arrow_pass), np.array(y_light_arrow_pass),
+                                        np.array(z_light_arrow_pass), linewidth=0.5, linestyle='-')
+    spin_pass_additional.append(plt_light_arrow_pass_ad)
+
+
 # Draw initial diagram
 update_diagram()
+draw_pass_spin()
 
 # Embed in Tkinter
 root = tk.Tk()
@@ -225,26 +258,18 @@ btn_reset = tk.Button(root, text="Reset", command=reset)
 btn_reset.pack(side='left')
 
 # Parameter setting
-frm_spin = ttk.Labelframe(root, relief='ridge', text='Spin', labelanchor='n')
+frm_spin = ttk.Labelframe(root, relief='ridge', text='Rotation', labelanchor='n')
 frm_spin.pack(side='left', fill=tk.Y)
-lbl_spin_n = tk.Label(frm_spin, text='Numerator:')
-lbl_spin_n.pack()
-var_spin_n = tk.StringVar(root)  # variable for spinbox-value
-var_spin_n.set(spin_numerator)  # Initial value
-spn_spin_n = tk.Spinbox(
-    frm_spin, textvariable=var_spin_n, from_=0, to=8, increment=1,
-    command=lambda: set_spin_n(var_spin_n.get()), width=6
-    )
-spn_spin_n.pack()
-lbl_spin_d = tk.Label(frm_spin, text='Denominator:')
-lbl_spin_d.pack()
-var_spin_d = tk.StringVar(root)  # variable for spinbox-value
-var_spin_d.set(spin_numerator)  # Initial value
-spn_spin_d = tk.Spinbox(
-    frm_spin, textvariable=var_spin_d, from_=0, to=8, increment=1,
-    command=lambda: set_spin_d(var_spin_d.get()), width=6
-    )
-spn_spin_d.pack()
+var_chk_axis_ts = tk.BooleanVar(root)    # Variable for checkbutton
+var_chk_axis_ts.set(True)
+chk_axis_ts = tk.Checkbutton(frm_spin, text="Time(t)-spatial(xy) (orange arrow)", 
+                             variable=var_chk_axis_ts, command=change_spin_state)
+chk_axis_ts.pack()
+var_chk_axis_ss = tk.BooleanVar(root)    # Variable for checkbutton
+var_chk_axis_ss.set(True)
+chk_axis_ss = tk.Checkbutton(frm_spin, text="Spatial(x)-spatial(y) (blue arrow)", 
+                             variable=var_chk_axis_ss, command=change_spin_state)
+chk_axis_ss.pack()
 
 # main loop
 anim = animation.FuncAnimation(fig, update, interval=100)
