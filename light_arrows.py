4063d8694afc2a52d13693908d7b721d6f3947c1
Marukatsu <szkktshr@gmail.com>
2024-12-08 08:18:17 +0900

    Add save_count=100

--- light_arrows.py
+++ light_arrows.py
@@ -223,5 +223,5 @@ rd_sw_on_spatial.pack()
 var_rd_sw.set(1)  # set default
 
 # Start main loop
-anim = animation.FuncAnimation(fig, update, interval=100)
+anim = animation.FuncAnimation(fig, update, interval=100, save_count=100)
 root.mainloop()
