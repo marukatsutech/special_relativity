8d46e1858cd66955da3c8d06ffce6a317ead3792
Marukatsu <szkktshr@gmail.com>
2025-03-31 21:32:36 +0900

    First commit.

---
+++ axes_rotation.py
@@ -0,0 +1,367 @@
+""" Axes rotation """
+import numpy as np
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.patches import Circle
+from scipy.spatial.transform import Rotation
+import mpl_toolkits.mplot3d.art3d as art3d
+from mpl_toolkits.mplot3d import proj3d
+
+
+""" Global variables """
+
+""" Animation control """
+is_play = False
+
+""" Axis vectors """
+vector_x_axis = np.array([1., 0., 0.])
+vector_y_axis = np.array([0., 1., 0.])
+vector_z_axis = np.array([0., 0., 1.])
+
+""" Other parameters """
+phase_deg = 0.
+phase_deg_step = 4.
+
+""" Create figure and axes """
+title_ax0 = "Axes rotation"
+title_tk = title_ax0
+
+x_min = -2.
+x_max = 2.
+y_min = -2.
+y_max = 2.
+z_min = -2.
+z_max = 2.
+
+fig = Figure()
+ax0 = fig.add_subplot(111, projection='3d')
+ax0.set_box_aspect((1, 1, 1))
+ax0.grid()
+ax0.set_title(title_ax0)
+ax0.set_xlabel("x")
+ax0.set_ylabel("y")
+ax0.set_zlabel("z")
+ax0.set_xlim(x_min, x_max)
+ax0.set_ylim(y_min, y_max)
+ax0.set_zlim(z_min, z_max)
+
+""" Embed in Tkinter """
+root = tk.Tk()
+root.title(title_tk)
+canvas = FigureCanvasTkAgg(fig, root)
+canvas.get_tk_widget().pack(expand=True, fill="both")
+
+toolbar = NavigationToolbar2Tk(canvas, root)
+canvas.get_tk_widget().pack()
+
+""" Global objects of Tkinter """
+var_rot_a = tk.BooleanVar(root)
+var_rot_b = tk.BooleanVar(root)
+var_rot_c = tk.BooleanVar(root)
+
+""" Classes and functions """
+
+
+class Counter:
+    def __init__(self, is3d=None, ax=None, xy=None, z=None, label=""):
+        self.is3d = is3d if is3d is not None else False
+        self.ax = ax
+        self.x, self.y = xy[0], xy[1]
+        self.z = z if z is not None else 0
+        self.label = label
+
+        self.count = 0
+
+        if not is3d:
+            self.txt_step = self.ax.text(self.x, self.y, self.label + str(self.count))
+        else:
+            self.txt_step = self.ax.text2D(self.x, self.y, self.label + str(self.count))
+            self.xz, self.yz, _ = proj3d.proj_transform(self.x, self.y, self.z, self.ax.get_proj())
+            self.txt_step.set_position((self.xz, self.yz))
+
+    def count_up(self):
+        self.count += 1
+        self.txt_step.set_text(self.label + str(self.count))
+
+    def reset(self):
+        self.count = 0
+        self.txt_step.set_text(self.label + str(self.count))
+
+    def get(self):
+        return self.count
+
+
+class ThreeAxes:
+    def __init__(self, ax=None, xyz=None):
+        self.ax = ax
+        self.xyz = xyz
+        self.is_rotate_xyz = False
+        self.phase_step_deg = 1
+        self.scale_a = 0
+        self.scale_b = 0
+        self.scale_c = 0
+
+        self.axis_a = np.array([1., 0., 0.])
+        self.axis_b = np.array([0., 1., 0.])
+        self.axis_c = np.array([0., 0., 1.])
+
+        self.x_axis_a = [self.axis_a[0], - self.axis_a[0]]
+        self.y_axis_a = [self.axis_a[1], - self.axis_a[1]]
+        self.z_axis_a = [self.axis_a[2], - self.axis_a[2]]
+        self.line_axis_a, = self.ax.plot(self.x_axis_a, self.y_axis_a, self.z_axis_a,
+                                         label='Axis A', color="red", ls="-.", linewidth=2)
+
+        self.x_axis_b = [self.axis_b[0], - self.axis_b[0]]
+        self.y_axis_b = [self.axis_b[1], - self.axis_b[1]]
+        self.z_axis_b = [self.axis_b[2], - self.axis_b[2]]
+        self.line_axis_b, = self.ax.plot(self.x_axis_b, self.y_axis_b, self.z_axis_b,
+                                         label='Axis B', color="green", ls="-.", linewidth=2)
+
+        self.x_axis_c = [self.axis_c[0], - self.axis_c[0]]
+        self.y_axis_c = [self.axis_c[1], - self.axis_c[1]]
+        self.z_axis_c = [self.axis_c[2], - self.axis_c[2]]
+        self.line_axis_c, = self.ax.plot(self.x_axis_c, self.y_axis_c, self.z_axis_c,
+                                         label='Axis C', color="blue", ls="-.", linewidth=2)
+
+        self.vector_a = self.ax.quiver(self.xyz[0], self.xyz[1], self.xyz[2],
+                                       self.axis_a[0] * self.scale_a, self.axis_a[1] * self.scale_a,
+                                       self.axis_a[2] * self.scale_a,
+                                       length=1, color="red", normalize=True, linewidth=3, alpha=1.0)
+
+        self.vector_b = self.ax.quiver(self.xyz[0], self.xyz[1], self.xyz[2],
+                                       self.axis_b[0] * self.scale_b, self.axis_b[1] * self.scale_b,
+                                       self.axis_b[2] * self.scale_b,
+                                       length=1, color="green", normalize=True, linewidth=3, alpha=1.0)
+
+        self.vector_c = self.ax.quiver(self.xyz[0], self.xyz[1], self.xyz[2],
+                                       self.axis_c[0] * self.scale_b, self.axis_c[1] * self.scale_b,
+                                       self.axis_c[2] * self.scale_b,
+                                       length=1, color="blue", normalize=True, linewidth=3, alpha=1.0)
+
+        self.x_a = []
+        self.y_a = []
+        self.z_a = []
+        self.path_a, = self.ax.plot(np.array(self.x_a), np.array(self.y_a), np.array(self.z_a),
+                                    color="red", linewidth=1)
+
+    def set_phase_step_deg(self, value):
+        self.phase_step_deg = value
+
+    def update_vectors(self):
+        self.vector_a.remove()
+        self.vector_b.remove()
+        self.vector_c.remove()
+
+        self.vector_a = self.ax.quiver(self.xyz[0], self.xyz[1], self.xyz[2],
+                                       self.axis_a[0] * self.scale_a, self.axis_a[1] * self.scale_a,
+                                       self.axis_a[2] * self.scale_a,
+                                       length=1, color="red", normalize=True, linewidth=3, alpha=1.0)
+
+        self.vector_b = self.ax.quiver(self.xyz[0], self.xyz[1], self.xyz[2],
+                                       self.axis_b[0] * self.scale_b, self.axis_b[1] * self.scale_b,
+                                       self.axis_b[2] * self.scale_b,
+                                       length=1, color="green", normalize=True, linewidth=3, alpha=1.0)
+
+        self.vector_c = self.ax.quiver(self.xyz[0], self.xyz[1], self.xyz[2],
+                                       self.axis_c[0] * self.scale_c, self.axis_c[1] * self.scale_c,
+                                       self.axis_c[2] * self.scale_c,
+                                       length=1, color="blue", normalize=True, linewidth=3, alpha=1.0)
+
+    def update_path(self):
+        if True:
+            self.x_a.append(self.axis_a[0])
+            self.y_a.append(self.axis_a[1])
+            self.z_a.append(self.axis_a[2])
+        self.path_a.set_xdata(np.array(self.x_a))
+        self.path_a.set_ydata(np.array(self.y_a))
+        self.path_a.set_3d_properties(np.array(self.z_a))
+
+    def rotate_all_axis_a(self):
+        rot_matrix = Rotation.from_rotvec(np.deg2rad(self.phase_step_deg) * self.axis_a)
+        # self.axis_a = rot_matrix.apply(self.axis_a)
+        self.axis_b = rot_matrix.apply(self.axis_b)
+        self.axis_c = rot_matrix.apply(self.axis_c)
+
+        self.update_axes()
+        self.update_vectors()
+
+    def rotate_all_axis_b(self):
+        rot_matrix = Rotation.from_rotvec(np.deg2rad(self.phase_step_deg) * self.axis_b)
+        self.axis_a = rot_matrix.apply(self.axis_a)
+        # self.axis_b = rot_matrix.apply(self.axis_b)
+        self.axis_c = rot_matrix.apply(self.axis_c)
+
+        self.update_axes()
+        self.update_vectors()
+
+    def rotate_all_axis_c(self):
+        rot_matrix = Rotation.from_rotvec(np.deg2rad(self.phase_step_deg) * self.axis_c)
+        self.axis_a = rot_matrix.apply(self.axis_a)
+        self.axis_b = rot_matrix.apply(self.axis_b)
+        # self.axis_c = rot_matrix.apply(self.axis_c)
+
+        self.update_axes()
+        self.update_vectors()
+
+    def update_axes(self):
+        self.x_axis_a = [self.axis_a[0], - self.axis_a[0]]
+        self.y_axis_a = [self.axis_a[1], - self.axis_a[1]]
+        self.z_axis_a = [self.axis_a[2], - self.axis_a[2]]
+        self.line_axis_a.set_data_3d(self.x_axis_a, self.y_axis_a, self.z_axis_a)
+
+        self.x_axis_b = [self.axis_b[0], - self.axis_b[0]]
+        self.y_axis_b = [self.axis_b[1], - self.axis_b[1]]
+        self.z_axis_b = [self.axis_b[2], - self.axis_b[2]]
+        self.line_axis_b.set_data_3d(self.x_axis_b, self.y_axis_b, self.z_axis_b)
+
+        self.x_axis_c = [self.axis_c[0], - self.axis_c[0]]
+        self.y_axis_c = [self.axis_c[1], - self.axis_c[1]]
+        self.z_axis_c = [self.axis_c[2], - self.axis_c[2]]
+        self.line_axis_c.set_data_3d(self.x_axis_c, self.y_axis_c, self.z_axis_c)
+
+    def clear_path(self):
+        self.x_a = []
+        self.y_a = []
+        self.z_a = []
+        self.update_path()
+
+    def reset(self):
+        self.xyz = np.array([0., 0., 0.])
+        self.axis_a = np.array([1., 0., 0.])
+        self.axis_b = np.array([0., 1., 0.])
+        self.axis_c = np.array([0., 0., 1.])
+
+        self.clear_path()
+        self.update_axes()
+        self.update_vectors()
+
+    def set_scale_a(self, value):
+        self.scale_a = value
+
+    def set_scale_b(self, value):
+        self.scale_b = value
+
+    def set_scale_c(self, value):
+        self.scale_c = value
+
+
+def set_phase_deg_step(value):
+    global phase_deg_step
+    phase_deg_step = value
+
+
+def create_parameter_setter():
+    global var_rot_a, var_rot_b, var_rot_c
+    # Rotations
+    frm_rot = ttk.Labelframe(root, relief="ridge", text="Rotations", labelanchor='n')
+    frm_rot.pack(side="left", fill=tk.Y)
+
+    # var_rot_a = tk.BooleanVar(root)
+    chk_rot_a = tk.Checkbutton(frm_rot, text="Axis A", variable=var_rot_a)
+    chk_rot_a.pack(anchor=tk.W)
+    var_rot_a.set(False)
+
+    # var_rot_b = tk.BooleanVar(root)
+    chk_rot_b = tk.Checkbutton(frm_rot, text="Axis B", variable=var_rot_b)
+    chk_rot_b.pack(anchor=tk.W)
+    var_rot_b.set(False)
+
+    # var_rot_c = tk.BooleanVar(root)
+    chk_rot_c = tk.Checkbutton(frm_rot, text="Axis C", variable=var_rot_c)
+    chk_rot_c.pack(anchor=tk.W)
+    var_rot_c.set(False)
+
+
+def create_animation_control():
+    frm_anim = ttk.Labelframe(root, relief="ridge", text="Animation", labelanchor="n")
+    frm_anim.pack(side="left", fill=tk.Y)
+    btn_play = tk.Button(frm_anim, text="Play/Pause", command=switch)
+    btn_play.pack(fill=tk.X)
+    btn_reset = tk.Button(frm_anim, text="Reset", command=reset)
+    btn_reset.pack(fill=tk.X)
+    btn_clear = tk.Button(frm_anim, text="Clear path", command=lambda: three_axes.clear_path())
+    btn_clear.pack(fill=tk.X)
+
+
+def create_center_lines():
+    line_axis_x = art3d.Line3D([0., 0.], [0., 0.], [z_min, z_max], color="gray", ls="-.", linewidth=1)
+    ax0.add_line(line_axis_x)
+    line_axis_y = art3d.Line3D([x_min, x_max], [0., 0.], [0., 0.], color="gray", ls="-.", linewidth=1)
+    ax0.add_line(line_axis_y)
+    line_axis_z = art3d.Line3D([0., 0.], [y_min, y_max], [0., 0.], color="gray", ls="-.", linewidth=1)
+    ax0.add_line(line_axis_z)
+
+
+def create_circle(ax, x, y, z, z_dir, edge_col, fill_flag, line_width, line_style, label):
+    if label != "":
+        c_spin_axis_guide = Circle((x, y), 1., ec=edge_col, fill=fill_flag,
+                                   linewidth=line_width, linestyle=line_style, label=label)
+    else:
+        c_spin_axis_guide = Circle((x, y), 1., ec=edge_col, fill=fill_flag,
+                                   linewidth=line_width, linestyle=line_style)
+    ax.add_patch(c_spin_axis_guide)
+    art3d.pathpatch_2d_to_3d(c_spin_axis_guide, z=z, zdir=z_dir)
+
+
+def draw_static_diagrams():
+    create_center_lines()
+    create_circle(ax0, 0., 0., 0., "x", "gray", False, 0.5,
+                  "--", "")
+    create_circle(ax0, 0., 0., 0., "y", "gray", False, 0.5,
+                  "--", "")
+    create_circle(ax0, 0., 0., 0., "z", "gray", False, 0.5,
+                  "--", "")
+
+
+def update_diagrams():
+    if var_rot_a.get():
+        three_axes.set_scale_a(1)
+        three_axes.rotate_all_axis_a()
+    else:
+        three_axes.set_scale_a(0)
+    if var_rot_b.get():
+        three_axes.set_scale_b(1)
+        three_axes.rotate_all_axis_b()
+    else:
+        three_axes.set_scale_b(0)
+    if var_rot_c.get():
+        three_axes.set_scale_c(1)
+        three_axes.rotate_all_axis_c()
+    else:
+        three_axes.set_scale_c(0)
+    three_axes.update_path()
+
+
+def reset():
+    global is_play, phase_deg
+    cnt.reset()
+    three_axes.reset()
+
+
+def switch():
+    global is_play
+    is_play = not is_play
+
+
+def update(f):
+    if is_play:
+        cnt.count_up()
+        update_diagrams()
+
+
+""" main loop """
+if __name__ == "__main__":
+    cnt = Counter(ax=ax0, is3d=True, xy=np.array([x_min, y_max]), z=z_max, label="Step=")
+    draw_static_diagrams()
+    create_animation_control()
+    create_parameter_setter()
+
+    three_axes = ThreeAxes(ax0, np.array([0., 0., 0.]))
+
+    ax0.legend(loc='lower right', fontsize=8)
+
+    anim = animation.FuncAnimation(fig, update, interval=100, save_count=100)
+    root.mainloop()
